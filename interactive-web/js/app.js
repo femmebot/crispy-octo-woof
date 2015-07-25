47a4c438cb27d8657035e51d18e75cec56a4159a
femmebot <femmebot@gmail.com>
2015-07-25 13:15:58 -0400

    +notes

---
+++ interactive-web/js/app.js
@@ -0,0 +1,36 @@
+// new-task
+// on button press, create a new list item from #new-task input
+  // input (checkbox)
+  // label
+  // input (text)
+  // button.edit
+  // button.delete
+  // each element needs modified and appended
+
+// incomplete-tasks
+// if checkbox is checked, move task to completed-tasks
+
+// if edit button pressed, switch to edit mode
+  // if the class of the parent is .editMode
+    // Switch from .editMode
+    // label text becomes the input's value
+  // else
+    // Switch to .editMode
+    // input value becomes the label's text
+
+  // toggle .editMode to the parent
+
+
+// if delete button clicked, delete entry
+  // When the Delete button is pressed
+    // Remove the parent list item from the ul
+
+
+// .completed-tasks
+  // When the checkbox is checked
+    // Append the task list item to the #completed-tasks
+
+// if checkbox unchecked, move task to #incomplete-tasks
+  // When the checkbox is unchecked
+    // Append the task list item to the #incomplete-tasks
+    
