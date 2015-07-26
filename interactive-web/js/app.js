6a165d79942c5c381bd675c3ced6fad5e06448dc
femmebot <femmebot@gmail.com>
2015-07-26 01:15:09 -0400

    +binding

--- interactive-web/js/app.js
+++ interactive-web/js/app.js
@@ -1,19 +1,20 @@
 
 var taskInput = document.getElementById("new-task"); // #new-task
-var addButton = document.getElementsByTagName("button")[0]; // first button
+var addButton = document.getElementsByTagName("button")[0]; //first button
 var incompleteTasks = document.getElementById("incomplete-tasks"); // incomplete task list
 var completedTasks = document.getElementById("completed-tasks"); // completed task list
 
-// new-task
+//Add a new task
 var addTask = function() {
-  // on button press, create a new list item from #new-task input
-    // input (checkbox)
-    // label
-    // input (text)
-    // button.edit
-    // button.delete
-    // each element needs modified and appended
-    console.log("add task…")
+  console.log("Add task...");
+  //When the button is pressed
+  //Create a new list item with the text from #new-task:
+  //input (checkbox)
+    //label
+    //input (text)
+    //button.edit
+    //button.delete
+    //Each elements, needs modified and appended
 }
 
 // incomplete-tasks
@@ -29,29 +30,29 @@ var editTask = function(){
     // input value becomes the label's text
 
   // toggle .editMode to the parent
-  console.log("edit task…")
+  console.log("edit task…");
 }
 
 var deleteTask = function() {
 // if delete button clicked, delete entry
   // When the Delete button is pressed
     // Remove the parent list item from the ul
-    console.log("delete task…")
+    console.log("delete task…");
 }
 
 var taskCompleted = function() {
 // .completed-tasks
   // When the checkbox is checked
     // Append the task list item to the #completed-tasks
-    console.log("task completed…")
+    console.log("task completed…");
 }
 
 var incompleteTask = function() {
 // if checkbox unchecked, move task to #incomplete-tasks
   // When the checkbox is unchecked
     // Append the task list item to the #incomplete-tasks
-    console.log("incomplete task…")
+    console.log("incomplete task…");
 }
 
 // Set the click handler to the addTask function
-addButton.onClick = addTask;
+addButton.onclick = addTask;
