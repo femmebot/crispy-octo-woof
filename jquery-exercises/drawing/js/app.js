484c09cc1c9854b55356166fcc349b53bf75b505
femmebot <femmebot@gmail.com>
2015-08-02 23:36:11 -0400

    +color switch, addfunctions

--- jquery-exercises/drawing/js/app.js
+++ jquery-exercises/drawing/js/app.js
@@ -1,8 +1,8 @@
 
 var $paintColor = $('.controls li');
-var $newColor = $('#revealColorSelect');
+var $newColorButton = $('#revealColorSelect');
 var $slidersInput = $('.sliders input');
-
+var $rgb;
 
 function switchPaintColor() {
   // console.log('switchPaintColor event called');
@@ -14,25 +14,44 @@ function toggleAddColorMenu() {
   $('#colorSelect').toggle();
 };
 
-function previewNewColor() {
-  var $rgb = 'rgb(' + $('#red').val();
+function addNewColor() {
+  $rgb = 'rgb(' + $('#red').val();
   $rgb += ', ' + $('#green').val();
   $rgb += ', ' + $('#blue').val() + ')';
-  $('#newColor').css('background-color', $rgb);
+  $('#newColor').css('background', $rgb);
+  return $rgb;
   // console.log($rgb);
 };
 
 // when user clicks on a new control color
   // deselect the current color
   // add .selected to the new color
-
 $paintColor.click(switchPaintColor);
 
 // when new color button #revealColorSelect is clicked
   // show/toggle the #colorSelect menu
-
-$newColor.click(toggleAddColorMenu);
+$newColorButton.click(toggleAddColorMenu);
 
 // when color sliders change, update the #newColor span
+$slidersInput.change(addNewColor);
 
-$slidersInput.change(previewNewColor);
+
+
+// when user clicks on #addNewColor button after previewing new color
+  // add new color <li> to .controls list
+  // select the newly-added color
+$('#addNewColor').click(function(){
+
+  $('.controls li').removeClass('selected');
+  $colorList = $('.controls ul').append('<li></li>');
+  $newColorClass = 'color' + $('.controls li').length;
+  $newColor = $('.controls li').last().addClass($newColorClass);
+
+  $newColor.css('background', $rgb);
+  $newColor.addClass('selected');
+  console.log($rgb);
+  console.log($newColorClass);
+
+});
+
+// on mouse events on the canvas, draw lines
