b750084b9a82b7df7dc20c0fa5ef1cc74cd82f68
femmebot <femmebot@gmail.com>
2015-08-03 01:21:16 -0400

    renamed functions

--- jquery-exercises/drawing/js/app.js
+++ jquery-exercises/drawing/js/app.js
@@ -2,11 +2,11 @@
 var $paintColor = $('.controls li');
 var $newColorButton = $('#revealColorSelect');
 var $slidersInput = $('.sliders input');
-var $rgb;
+// var $rgb;
 
 function switchPaintColor() {
   // console.log('switchPaintColor event called');
-  $paintColor.removeClass('selected');
+  $('.controls li').removeClass('selected');
   $(this).addClass('selected');
 };
 
@@ -14,44 +14,38 @@ function toggleAddColorMenu() {
   $('#colorSelect').toggle();
 };
 
-function addNewColor() {
+function getRGB() {
   $rgb = 'rgb(' + $('#red').val();
   $rgb += ', ' + $('#green').val();
   $rgb += ', ' + $('#blue').val() + ')';
   $('#newColor').css('background', $rgb);
   return $rgb;
-  // console.log($rgb);
 };
 
+function addNewColor(){
+  $('.controls li').removeClass('selected');
+  $colorList = $('.controls ul').append('<li></li>');
+  $newColorClass = 'color' + $('.controls li').length;
+  $newColor = $('.controls li').last().addClass($newColorClass);
+
+  $newColor.css('background', getRGB());
+  $newColor.addClass('selected');
+};
 // when user clicks on a new control color
   // deselect the current color
   // add .selected to the new color
-$paintColor.click(switchPaintColor);
+$('.controls li').click(switchPaintColor);
 
 // when new color button #revealColorSelect is clicked
   // show/toggle the #colorSelect menu
 $newColorButton.click(toggleAddColorMenu);
 
 // when color sliders change, update the #newColor span
-$slidersInput.change(addNewColor);
-
-
+$slidersInput.change(getRGB);
 
 // when user clicks on #addNewColor button after previewing new color
   // add new color <li> to .controls list
   // select the newly-added color
-$('#addNewColor').click(function(){
-
-  $('.controls li').removeClass('selected');
-  $colorList = $('.controls ul').append('<li></li>');
-  $newColorClass = 'color' + $('.controls li').length;
-  $newColor = $('.controls li').last().addClass($newColorClass);
-
-  $newColor.css('background', $rgb);
-  $newColor.addClass('selected');
-  console.log($rgb);
-  console.log($newColorClass);
-
-});
+$('#addNewColor').click(addNewColor);
 
 // on mouse events on the canvas, draw lines
