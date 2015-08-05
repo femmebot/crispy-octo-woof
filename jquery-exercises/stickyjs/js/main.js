284f326207382b3ddff23ade7985d16c5f9dd89b
femmebot <femmebot@gmail.com>
2015-08-05 11:14:45 -0400

    +ajax

--- jquery-exercises/stickyjs/js/main.js
+++ jquery-exercises/stickyjs/js/main.js
@@ -6,14 +6,19 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
-$('header').sticky().on('sticky-start', function(){
+$('header').sticky({
+  getWidthFrom: '.container',
+  responsiveWidth: true
+}).on('sticky-start', function(){
   $('.description').html('We build <strong>great</strong> apps');
 }).on('sticky-end', function(){
   $('.description').html('We build apps');
 });
 
 $('h5').sticky({
-  topSpacing: 60
+  topSpacing: 60,
+  getWidthFrom: '.container',
+  responsiveWidth: true
 }).on('sticky-start', function(){
   $(this).html('Want us to work on your project? <a href="#">Email us.</a>');
 }).on('sticky-end', function(){
