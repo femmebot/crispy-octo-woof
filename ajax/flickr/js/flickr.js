f1d0203713ebed003ea88f63a92ced28210f2535
femmebot <femmebot@gmail.com>
2015-08-09 02:09:33 -0400

    +flickr api

--- ajax/flickr/js/flickr.js
+++ ajax/flickr/js/flickr.js
@@ -8,10 +8,12 @@
 // flickrAPI is the URL to the public API
 // data we want to send to filter photos, etc that we get back
 // displayPhotos is the callback function that executes what we want to do with the response
-var $url = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';  // ?jsoncallback=? query string often used to get JSONP format
-var $photoHTML = $('#photos').append('<ul></ul>');
 
-$('.filter-select button').click(function() {
+
+$('.filter-select button').on('click', function() {
+
+  var $url = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';  // ?jsoncallback=? query string often used to get JSONP format
+  var $photoHTML = '<ul>';
   var $whichButton = $(this).text();
   var $flickrOptions = {
     tags: $whichButton,
@@ -21,28 +23,16 @@ $('.filter-select button').click(function() {
   $('button').removeClass('selected');
   $(this).addClass('selected');
 
-  function getPhotos(response) {
-    $.each(response.items,function(i,photo){
-      $photoHTML.append('<li class="grid-25 tablet-grid-50"></li>');
-      $photoHTML.append('<a href="' + photo.link + 'class="image">');
-      $photoHTML.append('<img src="' + photo.media.m + '">');
-    });
+  function displayPhotos(response) {
+    $.each(response.items,function(i,photo) {
+      $photoHTML += '<li class="grid-25 tablet-grid-50">';
+      $photoHTML += '<a href="' + photo.link + '" class="image">';
+      $photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+    }); // end each
+    $photoHTML += '</ul>';
+    $('#photos').html($photoHTML);
   };
-  $.getJSON($url, $flickrOptions, getPhotos);
-  // $('#photos').html($photoHTML);
-  console.log($photoHTML);
 
+  $.getJSON($url, $flickrOptions, displayPhotos);
+  // console.log($photoHTML);
 });
-
-
-
-
-  // $.getJSON( $url, $flickrOptions, function(response){
-  //   //$.each(array, function(index, item))
-  //   //$(array).each(function( index, element ){}
-  //   $(response).each(function(){
-  //     $photoHTML.append('<li class="grid-25 tablet-grid-50"></li>');
-  //     $photoHTML.append('<a href="' + response.media.m + 'class="image">');
-  //     console.log($photoHTML);
-  //   });
-  // });
